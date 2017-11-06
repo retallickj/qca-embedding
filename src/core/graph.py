720a5e235351a3d35f0f69f8527748ff52d997d9
Jake Retallick <retallick.jake@gmail.com>
2017-11-06 15:52:21 -0800

    Made Graph a derived class of networkx.Graph

--- src/core/graph.py
+++ src/core/graph.py
@@ -9,55 +9,38 @@ information. Base for the source graph structure in the embedding GUI.
 __author__      = 'Jake Retallick'
 __copyright__   = 'MIT License'
 __version__     = '2.0'
-__date__        = '2017-10-31'      # last update
+__date__        = '2017-11-06'      # last update
 
 from copy import copy
+import networkx as nx
 
-class Node(object):
-    '''Node class for Graph construction
-
-    Parameters:
-        n   : name or label for the node
-        adj : adjacency dict with adj[m] the edge weight between n and m
-        v   : value contained in the node
-        x   : optional x location information
-        y   : optional y location information
-    '''
-
-    def __init__(self, n, adj=dict(), v=0., x=None, y=None):
-        '''Construct a Node instance'''
-        self.n, self.adj, self.v, self.x, self.y = n, adj, v, x, y
-
-    def copy(self):
-        '''Return a deep copy of the node, assuming 'adj' is a dict of floats and
-        and other parameters are copyable '''
-        n, v, x, y = [copy(z) for z in [self.n, self.v, self.x, self.y]]
-        return Node(n, dict(self.adj), v, x, y)
-
-    def echo(self):
-        '''Human readable node information for debug'''
-
-        from pprint import pprint
-
-        print('Node: {0}::{1}'.format(self.n,self.v))
-        if self.x is not None:
-            print('Location: {0}::{1}'.format(self.x,self.y))
-        print('Adjacency:')
-        pprint(self.adj)
-        print('\n')
-        n.echo()
-
-
-
-class Graph(object):
-    '''General base class for qubit graphs'''
+class Graph(nx.Graph):
+    '''General base class for qubit graphs.'''
 
     file_delim = ' '    # graph file delimiter
 
-    def __init__(self):
-        '''Initialise a graph instance'''
+    def __init__(self, fn=None, nb=0, mp=int):
+        '''Initialise a Graph instance. If fn is give, initializes the Graph
+        from a graph file (see Graph.fromFile for details).
 
-        self.nodes = {}     # dictionary of nodes
+        parameters:
+            fn  : filename
+            nb  : number of header lines to burn before graph information
+            mp  : map to apply to node labels
+        '''
+
+        super(Graph, self).__init__()
+
+        if fn is not None:
+            try:
+                self.fromFile(fn, nb, mp)
+            except AssertionError as e:
+                print('Graph initialization failed with error:\n{0}'.format(e.message))
+                self.clear()
+
+    def clear(self):
+        '''Reset all data in the Graph'''
+        super(Graph, self).clear()  # clear nx.Graph data
 
     def fromFile(self, fn, nb=0, mp=int):
         '''Populate graph from file. Each line of the file must be a tuple of the
@@ -75,8 +58,9 @@ class Graph(object):
         assert isinstance(nb, int) and nb >= 0, 'Invalid burn count'
 
         # reset any existing graph information
-        self.nodes = {}
+        self.clear()
 
+        # read graph file
         try:
             with open(fn, 'r') as fp:
                 # burn header
@@ -86,50 +70,18 @@ class Graph(object):
         except IOError:
             print('Failed to read graph source file: {0}'.format(fn))
 
+    def subgraph(self, nodes, copy=False):
+        '''Return the sub-graph composed of the given list of nodes. The nodes
+        in the subgraph are deep-copies of those of the full graph. The returned
+        graph is of the same type as the Graph derived class.'''
 
-    def addNode(self, n, v=0., x=None, y=None):
-        '''Add an unconnected node to the Graph'''
+        G0 = nx.Graph(super(Graph, self).subgraph(nodes))
 
-        assert n not in self.nodes, 'Node already exists'
-        self.nodes[n] = Node(n, v=v, x=x, y=y)
-
-    def addEdge(self, n, m, v=0.):
-        '''Add an edge between two existing nodes'''
-
-        assert n in self.nodes and m in self.nodes, 'Missing node'
-        self.nodes[n].adj[m] = v
-        self.nodes[m].adj[n] = v
-
-    # graph methods
-
-    def size(self):
-        '''Return the number of nodes in the graph'''
-        return len(self.nodes)
-
-    def degree(self):
-        '''Return the degree of the graph'''
-        return max(len(n.adj) for n in self.nodes)
-
-    def number_of_nodes(self):
-        '''Return the number of nodes in the graph'''
-        return self.size()
-
-    def number_of_edges(self):
-        '''Return the number of edges in the graph'''
-        return int(sum(len(self.nodes[n].adj) for n in self.nodes)//2)
-
-    def subgraph(self, nodes):
-        '''Return the sub-graph composed of the given list of nodes.
-        The nodes in the subgraph are deep-copies of those of the full graph.'''
-
-        G = Graph()
-
-        # get copy of the included nodes
-        G.nodes = {k: self.nodes[k].copy() for k in nodes if k in self.nodes}
-
-        # remove extra edges
-        for k, node in G.nodes.items():
-            node.adj = {j: v for j,v in node.adj.items() if j in G.nodes}
+        # construct derived class and assign graph data
+        G = self.__new__(type(self))
+        G.graph = G0.graph  # graph attributes
+        G.node = G0.node    # dict of node dicts
+        G.edge = G0.edge    # dict of edge dicts
 
         return G
 
@@ -153,20 +105,18 @@ class Graph(object):
                 print('Invalid line format: {0} ... skipping'.format(line))
                 continue
             i,j = [mp(d) for d in data[:2]]
-            v,x,y = [float(d) if d is not None else None for d in data[2:]]
+            w,x,y = [float(d) if d is not None else None for d in data[2:]]
 
-            # add nodes if they do not exist
+            # add nodes
             for k in [i,j]:
-                if k not in self.nodes:
-                    self.nodes[k] = Node(k, adj={})
+                self.add_node(k)    # does nothing if node already exists
 
             # add node or edge information
             if i==j:
-                node = self.nodes[i]
-                node.v, node.x, node.y = v, x, y
+                self.add_node(i, weight=w, x=x, y=y)
             else:
-                self.nodes[i].adj[j] = v
-                self.nodes[j].adj[i] = v
+                self.add_edge(i, j, weight=w)
+
 
 if __name__ == '__main__':
 
@@ -181,7 +131,3 @@ if __name__ == '__main__':
 
     G = Graph()
     G.fromFile(fn, 1)
-
-    for k,n in G.nodes.items():
-        print('\n')
-        n.echo()
